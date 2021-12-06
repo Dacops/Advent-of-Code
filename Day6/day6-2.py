6ad6c11311fabb7f72f09a0485d8032d021a0c05
Dacops <79417054+Dacops@users.noreply.github.com>
2021-12-06 09:40:42 +0000

    day6

---
+++ Day6/day6-2.py
@@ -0,0 +1,28 @@
+def lanternfish(file):
+    with open(file,'r') as f:
+        lst=str(f.readlines())[2:-2].split(',')
+        
+    elements={'0':0,'1':0,'2':0,'3':0,'4':0,'5':0,'6':0,'7':0,'8':0}
+    for element in lst:
+        elements[element]+=1
+    for i in range(256):
+        born=elements['0']
+        elements['0']=elements['1']
+        elements['1']=elements['2']
+        elements['2']=elements['3']
+        elements['3']=elements['4']
+        elements['4']=elements['5']
+        elements['5']=elements['6']
+        elements['6']=elements['7']+born
+        elements['7']=elements['8']
+        elements['8']=born
+
+    return (elements['0']+
+            elements['1']+
+            elements['2']+
+            elements['3']+
+            elements['4']+
+            elements['5']+
+            elements['6']+
+            elements['7']+
+            elements['8'])
