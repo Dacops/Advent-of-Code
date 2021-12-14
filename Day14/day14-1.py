9e9a93e9228407d228c1350638b8390ca91c851d
Dacops <79417054+Dacops@users.noreply.github.com>
2021-12-14 10:25:20 +0000

    day14

--- Day14/day14-1.py
+++ Day14/day14-1.py
@@ -1,9 +1,40 @@
 def function(file):
     with open(file,'r') as f:
         lst=f.readlines()
-    for l in lst: l.rstrip()
+    polymer=lst[0].rstrip()
+    instructions=[]
+    for instruction in lst[1:]:
+        if instruction != '\n': instructions.append(instruction.rstrip())
+    polymers, enter = [], []
+    for instruction in instructions:
+        pol, ent = instruction.split(' -> ')
+        polymers.append(pol)
+        enter.append(ent)
 
+    def grow_polymer(polymer, count):
+        ant, inside = None, []
+        for letter in polymer:
+            if ant!=None:
+                if ant+letter in polymers:
+                    ins = enter[polymers.index(ant+letter)]
+                    inside.append(ins)
+            ant=letter
+    
+        new_polymer=polymer[0]
+        for i in range(len(inside)):
+            new_polymer+=inside[i]+polymer[i+1]
+        
+        count+=1
+        if count<10: return grow_polymer(new_polymer, count)
+        else:
+            d={}
+            for letter in new_polymer:
+                if letter not in d: d[letter]=0
+                if letter in d: d[letter]+=1
+            values=[]
+            for element in d:
+                values.append(d[element])
+            values = sorted(values)
+            return values[-1]-values[0]
 
-
-
-print(function('C:\\Users\\david\\OneDrive\\Desktop\\GitHub\\AoC_21\\Day14\\day14_INPUT.txt'))
+    return grow_polymer(polymer, 0)
--- Day14/day14-2.py
+++ Day14/day14-2.py
@@ -1,9 +1,44 @@
 def function(file):
     with open(file,'r') as f:
         lst=f.readlines()
-    for l in lst: l.rstrip()
+    polymer=lst[0].rstrip()
+    instructions=[]
+    for instruction in lst[1:]:
+        if instruction != '\n': instructions.append(instruction.rstrip())
+    polymers, enter = [], []
+    for instruction in instructions:
+        pol, ent = instruction.split(' -> ')
+        polymers.append(pol)
+        enter.append(ent)
+    
+    ant, poly = None, {}
+    for letter in polymer:
+        if ant!=None:
+            if ant+letter not in poly: poly[ant+letter]=0
+            if ant+letter in poly: poly[ant+letter]+=1
+        ant=letter
 
+    def grow_polymer(poly, count):
+        poly_c = {}
+        for group in poly:
+            first_half, second_half = group[0]+enter[polymers.index(group)], enter[polymers.index(group)]+group[1]
+            if first_half not in poly_c: poly_c[first_half]=0
+            if first_half in poly_c: poly_c[first_half]+=poly[group]
+            if second_half not in poly_c: poly_c[second_half]=0
+            if second_half in poly_c: poly_c[second_half]+=poly[group]
+        count+=1
+        if count<40: return grow_polymer(poly_c, count)
+        else:
+            values={}
+            for key in poly_c:
+                if key[0] not in values: values[key[0]]=0
+                if key[0] in values: values[key[0]]+=poly_c[key]
+                if key[1] not in values: values[key[1]]=0
+                if key[1] in values: values[key[1]]+=poly_c[key]
+            new_values = []
+            for value in values:
+                new_values.append(values[value]/2)
+            new_values = sorted(new_values)
+            return round(new_values[-1]-new_values[0])
 
-
-
-print(function('C:\\Users\\david\\OneDrive\\Desktop\\GitHub\\AoC_21\\Day14\\day14_INPUT.txt'))
+    return grow_polymer(poly, 0)
