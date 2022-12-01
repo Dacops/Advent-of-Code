abf1104302af645e0035ad86afad45a631608a5c
David Pires <79417054+Dacops@users.noreply.github.com>
2022-12-01 22:44:51 +0000

    day 1

---
+++ AoC_22/Day_1/day1-1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> _calories;                  // index = elf; value = num of calories;
+int elf = 0;
+
+void readInput() {
+
+    string line;
+    _calories.resize(sizeof(int));
+
+    while (getline(cin, line)) {
+
+        if (int(line[0])==13) {         // Vertical Tab, ASCII code: 13;
+            elf++;
+            _calories.resize((elf+1)*sizeof(int));
+        }
+        else { _calories[elf]+=stoi(line); }
+    }
+}
+
+int evaluateInput() {
+
+    int max = 0;
+
+    for (int i=0; i<elf; i++) {
+        if (max < _calories[i]) { max = _calories[i]; }
+    }
+    return max;
+}
+
+
+int main() {
+    readInput();
+    cout << evaluateInput() << endl;
+
+    return 0;
+}
---
+++ AoC_22/Day_1/day1-2.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+
+using namespace std;
+
+
+vector<int> _calories;                  // index = elf; value = num of calories;
+int elf = 0;
+
+void readInput() {
+
+    string line;
+    _calories.resize(sizeof(int));
+
+    while (getline(cin, line)) {
+
+        if (int(line[0])==13) {         // Vertical Tab, ASCII code: 13;
+            elf++;
+            _calories.resize((elf+1)*sizeof(int));
+        }
+        else { _calories[elf]+=stoi(line); }
+    }
+}
+
+int evaluateInput() {
+
+    vector<int> max{0, 0, 0};
+
+    for (int i=0; i<=elf; i++) {
+        if (max[0] < _calories[i]) {
+            max[0] = _calories[i];
+            sort(max.begin(), max.end());
+        }
+    }
+    return max[0]+max[1]+max[2];
+}
+
+
+int main() {
+    readInput();
+    cout << evaluateInput() << endl;
+
+    return 0;
+}
