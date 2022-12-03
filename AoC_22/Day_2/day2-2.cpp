5285aafaa57df27ecf2a0a717486ac1c76382f4d
David Pires <79417054+Dacops@users.noreply.github.com>
2022-12-03 20:53:14 +0000

    day2 optimization

--- AoC_22/Day_2/day2-2.cpp
+++ AoC_22/Day_2/day2-2.cpp
@@ -6,14 +6,6 @@ using namespace std;
 
 
 vector<vector<char>> _inputs;
-map<char, int> _values = {
-        { 'X', 1 }, { 'Y', 2 }, { 'Z', 3 }
-};
-map<string, int> _wins = {
-        { "AX", 3 }, { "BX", 0 }, { "CX", 6 },
-        { "AY", 6 }, { "BY", 3 }, { "CY", 0 },
-        { "AZ", 0 }, { "BZ", 6 }, { "CZ", 3 }
-};
 map<char, char> _lose = {
         { 'A', 'Z'}, { 'B', 'X'}, { 'C', 'Y' }
 };
@@ -52,16 +44,12 @@ void changeInput() {
 int evaluateInput() {
 
     int score = 0;
-    string current;
+    vector<int> vals = {3,6,0};
 
     for (int i=0; i<(int)_inputs.size()-1; i++) {
-
-        current = "";
-        current = current + _inputs[i][0] + _inputs[i][1];
-        score += _values[_inputs[i][1]];
-        score += _wins[current];
+        score+=( (int(_inputs[i][1])-('X'-1)) +
+                 vals[ (3 + (int(_inputs[i][1])-'X') - (int(_inputs[i][0])-'A')) % 3 ]);
     }
-
     return score;
 }
 
