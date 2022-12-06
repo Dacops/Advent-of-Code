dc0b97169ead5cbce6a73ab3cb4fc95f0c823899
David Pires <79417054+Dacops@users.noreply.github.com>
2022-12-06 12:37:47 +0000

    day6

---
+++ AoC_22/Day_6/day6-2.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+
+const int distinct = 14;
+
+int diff(vector<char> chars) {
+    for(int j=0; j<(int)chars.size(); j++) {
+        for (int i=0; i < (int) chars.size(); i++) {
+            if (i!=j && chars[j]==chars[i]) return 0;
+        }
+    }
+    return 1;
+}
+
+
+int readInput() {
+
+    vector<char> chars;
+    chars.resize(distinct);
+    char curr;
+    int i=0;
+
+    while(cin>>curr) {
+        chars[i%distinct]=curr;
+        i++;
+
+        if(i>distinct && diff(chars))  break;
+    }
+    return i;
+}
+
+
+
+int main() {
+    cout << readInput() << endl;
+
+    return 0;
+}
