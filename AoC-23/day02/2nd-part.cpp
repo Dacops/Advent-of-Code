0bac81ad9b3e906b868e5f05f029bc234cc813a1
David Pires <79417054+Dacops@users.noreply.github.com>
2023-12-02 05:36:43 +0000

    day02

---
+++ AoC-23/day02/2nd-part.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+#include <string>
+#include <unordered_map>
+
+using namespace std;
+
+// initialization lambda funtion, 
+// do nothing -> call for optimizations
+auto init = []()
+{ 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    return 0;
+}();
+
+
+unordered_map<string, int> colours = { {"blue", 14}, {"red", 12}, {"green", 13} };
+
+int evaluateGame(const string& game) {
+
+    unordered_map<string, int> maxColour = { {"blue", 0}, {"red", 0}, {"green", 0} };
+
+    for (const auto& colour : colours) {
+        int pos = game.find(colour.first);
+        while (pos != (int)string::npos) {
+            
+            int number = 0;
+            if (game[pos-3] >= 49 && game[pos-3] <= 57) {
+                number = (game[pos-3] - 48)*10;
+            }
+            number += game[pos-2] - 48;
+            
+            if (number > maxColour[colour.first]) {
+                maxColour[colour.first] = number;
+            }
+
+            pos = game.find(colour.first, pos+1);
+        }
+    }
+
+    return maxColour["blue"] * maxColour["red"] * maxColour["green"];
+}
+
+int evaluateInput() {
+
+    string line;
+    int answer = 0, game = 0;
+
+    while (getline(cin, line)) {
+
+        game++;
+        answer += evaluateGame(line);    
+    }
+
+    return answer;
+}
+
+
+int main() {
+    cout << evaluateInput() << endl;
+
+    return 0;
+}
