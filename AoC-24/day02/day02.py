b3b7183953719dcb1cd1bba7173d1aa4c4a930b5
David Pires <david.cq.pires@gmail.com>
2024-12-02 12:59:39 +0100

    day2

---
+++ AoC-24/day02/day02.py
@@ -0,0 +1,59 @@
+def valid_line(line):
+    increasing = None
+    for i in range(len(line)-1):
+        drop = int(line[i])-int(line[i+1])
+        if abs(drop) < 1 or abs(drop) > 3:
+            return False
+        elif drop < 0:
+            if increasing == None:
+                increasing = True
+            if increasing == False:
+                return False
+        else:
+            if increasing == None:
+                increasing = False
+            if increasing == True:
+                return False
+    return True
+
+def star1(file):
+    with open(file, 'r') as f:
+        vals = f.readlines()
+
+    safe = 0
+    for line in vals:
+        line = line.strip()
+        line = line.split(' ')
+
+        if valid_line(line):
+            safe += 1
+
+    return safe
+
+def star2(file):
+    with open(file, 'r') as f:
+        vals = f.readlines()
+
+    safe = 0
+    for line in vals:
+        line = line.strip()
+        line = line.split(' ')
+
+        if valid_line(line):
+            safe += 1
+
+        else:
+            # bruteforce removing levels
+            lines = []
+            for i in range(len(line)):
+                lines.append(line[:i] + line[i+1:])
+
+            for line in lines:
+                if valid_line(line):
+                    safe += 1
+                    break
+    
+    return safe
+
+print(f"STAR 1: {star1('input.txt')}")
+print(f"STAR 2: {star2('input.txt')}")
