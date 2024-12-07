a6e453c5833b30d47c45c429f33a6ea80d9e89a7
David Pires <david.cq.pires@gmail.com>
2024-12-07 10:14:38 +0100

    day7

--- AoC-24/day07/day07.py
+++ AoC-24/day07/day07.py
@@ -1,182 +1,56 @@
+def recurs(past, vals, target, part2):
+    new_past = []
+    for p in past:
+        if p > target:
+            continue
+        new_past.append(p * int(vals[0]))
+        new_past.append(p + int(vals[0]))
+        if part2:
+            new_past.append(int(str(p) + vals[0]))
+    
+    if len(vals[1:]) == 0:
+        return new_past
+    
+    return recurs(new_past, vals[1:], target, part2)
+
+
 def star1(file):
     with open(file, 'r') as f:
         lines = f.readlines()
 
-    lab = []
-    for line in lines:
-        line = line.strip()
-        lab.append(list(line))
+    eqs = []
+    for l in lines:
+        (val, vals) = l.strip().split(":")
+        vals = vals.strip().split(" ")
+        eqs.append((val, vals))
 
-    x, y = 0, 0
-    for i in range(len(lab)):
-        for j in range(len(lab[i])):
-            if lab[i][j] == '^':
-                y, x = i, j
-                break
+    correct = 0
+    for (val, vals) in eqs:
+        returns = recurs([int(vals[0])], vals[1:], int(val), False)
+        if int(val) in returns:
+            correct += int(val)
 
-    direction = 'up'
-    while True:
-        if direction == 'up':
-            if y-1 < 0:
-                lab[y][x] = 'X'
-                break
-            if lab[y-1][x] == '#':
-                direction = 'right'
-            else:
-                lab[y][x] = 'X'
-                y -= 1
-
-        elif direction == 'right':
-            if x+1 >= len(lab[y]):
-                lab[y][x] = 'X'
-                break
-            if lab[y][x+1] == '#':
-                direction = 'down'
-            else:
-                lab[y][x] = 'X'
-                x += 1
-
-        elif direction == 'down':
-            if y+1 >= len(lab):
-                lab[y][x] = 'X'
-                break
-            if lab[y+1][x] == '#':
-                direction = 'left'
-            else:
-                lab[y][x] = 'X'
-                y += 1
-
-        elif direction == 'left':
-            if x-1 < 0:
-                lab[y][x] = 'X'
-                break
-            if lab[y][x-1] == '#':
-                direction = 'up'
-            else:
-                lab[y][x] = 'X'
-                x -= 1
-
-    steps = 0
-    for l in lab:
-        steps += l.count('X')
-    return steps
-
-def check_loops(lab, x, y, direction, nob):
-    
-    nlab = [list(l) for l in lab]
-    nlab[nob[1]][nob[0]] = '#'
-    times = 0
-    while True:
-
-        if times > 5774:
-            return 1
-        times += 1
-
-        if direction == 'up':
-            if y-1 < 0:
-                return 0
-            if nlab[y-1][x] == '#':
-                direction = 'right'
-            else:
-                y -= 1
-
-        elif direction == 'right':
-            if x+1 >= len(nlab[y]):
-                return 0
-            if nlab[y][x+1] == '#':
-                direction = 'down'
-            else:
-                x += 1
-
-        elif direction == 'down':
-            if y+1 >= len(nlab):
-                return 0
-            if nlab[y+1][x] == '#':
-                direction = 'left'
-            else:
-                y += 1
-
-        elif direction == 'left':
-            if x-1 < 0:
-                return 0
-            if nlab[y][x-1] == '#':
-                direction = 'up'
-            else:
-                x -= 1
+    return correct
 
 
 def star2(file):
     with open(file, 'r') as f:
         lines = f.readlines()
 
-    lab = []
-    for line in lines:
-        line = line.strip()
-        lab.append(list(line))
+    eqs = []
+    for l in lines:
+        (val, vals) = l.strip().split(":")
+        vals = vals.strip().split(" ")
+        eqs.append((val, vals))
 
-    x, y = 0, 0
-    for i in range(len(lab)):
-        for j in range(len(lab[i])):
-            if lab[i][j] == '^':
-                y, x = i, j
-                break
+    correct = 0
+    for (val, vals) in eqs:
+        returns = recurs([int(vals[0])], vals[1:], int(val), True)
+        if int(val) in returns:
+            correct += int(val)
 
-    direction, previous, visited = 'up', set(), set()
-    visited.add((x, y))
-    while True:
-        if direction == 'up':
-            if y-1 < 0:
-                lab[y][x] = 'X'
-                break
-            if lab[y-1][x] == '#':
-                direction = 'right'
-            else:
-                if (x, y-1) not in previous and (x, y-1) not in visited and check_loops(lab, x, y, direction, (x, y-1)):
-                    previous.add((x, y-1))
-                visited.add((x, y))
-                lab[y][x] = 'X'
-                y -= 1
+    return correct
 
-        elif direction == 'right':
-            if x+1 >= len(lab[y]):
-                lab[y][x] = 'X'
-                break
-            if lab[y][x+1] == '#':
-                direction = 'down'
-            else:
-                if (x+1, y) not in previous and (x+1, y) not in visited and check_loops(lab, x, y, direction, (x+1, y)):
-                    previous.add((x+1, y))
-                visited.add((x, y))
-                lab[y][x] = 'X'
-                x += 1
-
-        elif direction == 'down':
-            if y+1 >= len(lab):
-                lab[y][x] = 'X'
-                break
-            if lab[y+1][x] == '#':
-                direction = 'left'
-            else:
-                if (x, y+1) not in previous and (x, y+1) not in visited and check_loops(lab, x, y, direction, (x, y+1)):
-                    previous.add((x, y+1))
-                visited.add((x, y))
-                lab[y][x] = 'X'
-                y += 1
-
-        elif direction == 'left':
-            if x-1 < 0:
-                lab[y][x] = 'X'
-                break
-            if lab[y][x-1] == '#':
-                direction = 'up'
-            else:
-                if (x-1, y) not in previous and (x-1, y) not in visited and check_loops(lab, x, y, direction, (x-1, y)):
-                    previous.add((x-1, y))
-                visited.add((x, y))
-                lab[y][x] = 'X'
-                x -= 1
-
-    return len(previous)
 
 print(f"STAR 1: {star1('input.txt')}")
 print(f"STAR 2: {star2('input.txt')}")
